f0cba820222c6601abcb63e7171d984987933246
pedro <pedro.adoneto@gmail.com>
2023-08-21 12:24:43 -0300

    heap array being populated

--- components/malloc/malloc.cpp
+++ components/malloc/malloc.cpp
@@ -1,8 +1,27 @@
 #include "malloc.hpp"
 #include <stdio.h>
 
+Block_t *get_heap() {
+    Block_t *heap = new Block_t[100];
+
+    for (size_t i = 0; i < 100; i++) {
+        heap[i].size = i;
+    }
+
+    return heap;
+}
+
 void *malloc_redefine(size_t size) {
-    printf("malloc redefine\n");
+    printf("malloc_redefine started\n");
+
+    Block_t *heap = get_heap();
+
+    printf("heap: ");
+
+    for (size_t i = 0; i < 100; i++) {
+        printf("%x, ", heap[i].size);
+    }
+
     void *ptr = 0x0;
     return ptr;
 }
--- components/malloc/malloc.hpp
+++ components/malloc/malloc.hpp
@@ -1,3 +1,10 @@
 #include <cstddef>
+#include <stdint.h>
+
+struct Block_t {
+    uint32_t size;
+    Block_t *next;
+    Block_t *previous;
+};
 
 void *malloc_redefine(size_t size);
